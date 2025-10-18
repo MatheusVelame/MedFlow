230540d897860251dc0de999f0ac5b21fd24ca95
luzia <luzia@DESKTOP-7NR9K66>
2025-10-18 07:33:17 -0300

    Adicionando cenários, steps e runner de Tipos de Exames

---
+++ medflow-bdd-tipos-exames/src/test/java/com/medflow/bdd/runner/RunCucumberTiposExamesTest.java
@@ -0,0 +1,8 @@
+package com.medflow.bdd.runner;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+public class RunCucumberTiposExamesTest {
+    // vazio: a descoberta é feita pelo engine do Cucumber
+}
